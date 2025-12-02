262b084711400d3cd42e310862599ebe415921d5
shri2004-AI <hs.shri2004@gmail.com>
2025-12-02 22:22:44 +0530

    in this lecture i learned datatypes, varriable ,and varriable naming convention

---
+++ Lecture55/dataType.js
@@ -0,0 +1,25 @@
+let x="harsh";
+let y=22;
+let z=3.55;
+const p=true;
+let q=undefined;
+let r=null;
+
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+
+
+let obj={
+    name:"harsh",
+    "job role":4556,
+    isHandsome:true
+}
+
+console.log(obj);
+
+obj.salary="2croes"
+
+console.log(obj);
+
+
---
+++ Lecture55/script.js
@@ -0,0 +1,23 @@
+console.log("it is the tutorial 55 ");
+
+
+var a=5;
+var b=6;
+var c="harsh"
+
+// var 55a ="harry" ; // not allowed
+console.log(a+c+b);
+
+console.log(typeof a,typeof b,typeof c)
+
+
+// const a1=6;
+// a1=a1+2;   //not allowed because a1 is constant
+
+
+{
+    let a=66;
+    console.log(a)
+}
+
+console.log(a)
