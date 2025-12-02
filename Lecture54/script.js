74612d4212ef0d6057555f0d4e8f87a193b50584
shri2004-AI <hs.shri2004@gmail.com>
2025-12-02 21:47:25 +0530

    i learned javascript basic.

---
+++ Lecture54/script.js
@@ -0,0 +1,24 @@
+alert("hello coder")
+
+console.log("hey i am console ...");
+console.log("hey i am running console ...");
+
+
+var a =prompt("enter your number")
+
+console.log("your number is " + a);
+
+var isTrue=confirm("are you sure you want to leave this page and blast your computer. ")
+
+
+if(isTrue){
+    console.log("computer is blasting");
+}
+else{
+    console.log("computer is not blasting");
+}
+
+
+document.title="first js program"
+
+document.body.style.backgroundColor="blue"
