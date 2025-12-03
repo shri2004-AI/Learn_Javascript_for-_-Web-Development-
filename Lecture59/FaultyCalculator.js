29aaf9769c046ea185d401b7c7ace8e922935314
shri2004-AI <hs.shri2004@gmail.com>
2025-12-03 10:47:51 +0530

    In this lecture I have create faulty calculator .

---
+++ Lecture59/FaultyCalculator.js
@@ -0,0 +1,50 @@
+/*    create a faulty calculator using javascript
+        
+This faulty calculator does following :
+1. it takes two numbers as input from the user 
+2. it performs wrong operations as follows:
+     +  ----> -
+     *  ----> +
+     -  ----> /
+     /  ----> **
+
+     it performs wrong operation 10% of the times
+
+*/
+
+
+let random=Math.random();
+let first=18;
+let secound=10;
+
+let operator=prompt("enter the operator ");
+
+let result
+if(random>0.1){
+     if(operator==="+"){
+          result=first+secound
+     }
+     else if(operator==="-"){
+          result=first-secound
+     }
+     else if(operator==="*"){
+          result=first*secound
+     }else if(operator==="/"){
+          result=first-secound
+     }
+}
+else{
+    if(operator==="+"){
+          result=first-secound
+     }
+     else if(operator==="-"){
+          result=first/secound
+     }
+     else if(operator==="*"){
+          result=first+secound
+     }else if(operator==="/"){
+          result=first**secound
+     } 
+}
+
+console.log("result : "+result);
