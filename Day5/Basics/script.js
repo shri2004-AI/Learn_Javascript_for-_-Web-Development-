583888a02effe9085549f4f8eae12c8c4a48a12f
shri2004-AI <hs.shri2004@gmail.com>
2025-12-25 14:51:09 +0530

    in this lecture learn the basics of functions

---
+++ Day5/Basics/script.js
@@ -0,0 +1,61 @@
+// function hello(){
+//     console.log("hello coder");
+// }
+
+// hello(); 
+
+// function print1To5(){
+//     for(let i=1;i<=5;i++){
+//         console.log(i);
+//     }
+// }
+// print1To5();
+
+// function isAdult(){
+//     let age=12;
+//     if(age>=18){
+//         console.log("adult");
+//     }else{
+//         console.log("not adult");
+//     }
+// }
+// isAdult();
+
+// function with arguments
+// function printName(name){
+//      console.log(name);
+// }
+// printName("Harsh");
+
+// function printNameAge(name,age){
+//      console.log(`${name}'s age is ${age}.`);
+// }
+// printNameAge("Harsh",23);
+
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// sum(4,9);
+
+
+//return keyword
+// function sum(a,b){
+//     return a+b;
+//     console.log("hello");
+// }
+// let sum1=sum(4,7);
+// console.log(sum1);
+
+// let sum2=sum(sum(3,5),7);
+// console.log(sum2);
+
+
+// function isAdult(age){
+//     if(age>=18){
+//         console.log("is adult");
+//     }
+//     else{
+//         console.log("is not adult");
+//     } 
+// }
+
