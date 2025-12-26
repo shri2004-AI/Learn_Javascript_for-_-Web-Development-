d452bad94434f8243094d14877c318df1e4e7745
shri2004-AI <hs.shri2004@gmail.com>
2025-12-26 23:48:11 +0530

    in this video ,i am learn set-timeout and set-interval function.

---
+++ Day6/setTimeOutAndIntervalFunction/script.js
@@ -0,0 +1,27 @@
+// setTimeout function
+// console.log("hii there");
+
+// setTimeout( ()=>{
+//     console.log("apna college");
+// },10000);
+
+// console.log("welcome to..");
+
+
+// setInterval function
+console.log("hii there");
+
+let id1=setInterval(() => {
+    console.log("apna college");
+}, 2000);
+
+console.log("welcome to..");
+console.log(id1);
+
+
+let id2=setInterval(() => {
+    console.log("apni duniya");
+}, 2000);
+
+console.log("welcome to..");
+console.log(id2);
---
+++ Day6/setTimeOutAndIntervalFunction/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+console.log("hii there");
+
+// setTimeout( ()=>{
+//     console.log("apna college");
+// },10000);
+
+// console.log("welcome to..");
