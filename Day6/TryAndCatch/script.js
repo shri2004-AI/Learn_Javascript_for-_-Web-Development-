964f3eb7597ba303f412e8efbff2c3d53cc191a7
shri2004-AI <hs.shri2004@gmail.com>
2025-12-26 23:45:00 +0530

    in this lecture i am learn try and catch statement

---
+++ Day6/TryAndCatch/script.js
@@ -0,0 +1,15 @@
+console.log("hello");
+console.log("hello");
+console.log("hello");
+
+try {
+    console.log(a);
+} catch (err) {
+    console.log("caught an error... a is not defined.");
+    console.log(err);
+
+}
+
+console.log("hello");
+console.log("hello");
+console.log("hello");
