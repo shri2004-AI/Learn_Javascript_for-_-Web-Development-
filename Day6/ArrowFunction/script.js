37aa711c9d5ad560e24e3c3a020e7ded7b4b17d5
shri2004-AI <hs.shri2004@gmail.com>
2025-12-26 23:46:02 +0530

    in this lecture ,i am learn Arrow function

---
+++ Day6/ArrowFunction/script.js
@@ -0,0 +1,28 @@
+// const printSum =(a,b)=>{
+//     console.log(a+b);
+// }
+
+// printSum(2,5);
+
+
+// const cube =(a)=>{
+//    return a*a*a;
+// };
+
+// console.log(cube(4));
+
+
+// const hello=() => {
+//       console.log("hello coder");
+// };
+
+// hello();
+
+
+// implicit return
+// const mul=(a,b) =>(
+//     a*b
+// );
+
+const mul=(a,b) =>(a*b);
+console.log(mul(3,7));
