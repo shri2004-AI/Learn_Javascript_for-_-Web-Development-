202ce0d435f75b004c066b5f840574834b8813d1
shri2004-AI <hs.shri2004@gmail.com>
2025-12-30 09:37:59 +0530

    how to acess previous elment sibiling/next element sibiling and also learn how to select parentelement and children element

---
+++ Day8(Dom)/Navigation/script.js
@@ -0,0 +1,18 @@
+// let H2=document.querySelector('h2');
+// console.dir(H2.parentElement);
+// console.dir(H2.children);
+// console.dir(H2.childElementCount);
+
+
+let ul=document.querySelector('ul');
+console.dir(ul.parentElement);
+console.dir(ul.children);
+console.dir(ul.childElementCount);
+
+console.dir(ul.children[2].previousElementSibling);
+console.dir(ul.children[2].nextElementSibling);
+
+
+let img=document.querySelector('img');
+console.dir(img.previousElementSibling);
+console.dir(img.nextElementSibling);
