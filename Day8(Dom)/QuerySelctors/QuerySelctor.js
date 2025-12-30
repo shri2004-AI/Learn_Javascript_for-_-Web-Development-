2779f9de593a47b6821cb115eccc81e1182d7f5f
shri2004-AI <hs.shri2004@gmail.com>
2025-12-30 09:33:14 +0530

    i am learn queryselector

---
+++ Day8(Dom)/QuerySelctors/QuerySelctor.js
@@ -0,0 +1,7 @@
+console.dir(document.querySelector("h3"));
+console.dir(document.querySelector("#heading"));
+console.dir(document.querySelector("div p"));
+
+
+// select all paragraph using querySelector all
+console.dir(document.querySelectorAll("h3"));
