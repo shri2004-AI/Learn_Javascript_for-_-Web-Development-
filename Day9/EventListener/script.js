73eace48cae8064200e11e98b27b3a2ce018ed55
shri2004-AI <hs.shri2004@gmail.com>
2025-12-31 15:10:25 +0530

    in this lecture ,i learn eventListener

---
+++ Day9/EventListener/script.js
@@ -0,0 +1,25 @@
+ 
+let btns=document.querySelectorAll("button");
+for(btn of btns){
+    btn.addEventListener("click",sayHello);
+    btn.addEventListener("click",sayName);
+};
+
+function sayHello(){
+    console.log(("hello"));
+};
+function sayName(){
+    console.log("Apna College");
+};
+
+
+
+let p=document.querySelector("p");
+p.addEventListener("click",function(){
+    console.log("pera was clicked.");
+})
+
+let box=document.querySelector("div");
+box.addEventListener("mouseenter",function(){
+    console.log("div was clicked.");
+})
