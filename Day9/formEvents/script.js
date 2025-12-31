368d7fd5d818b843053d509766e66a06ae990213
shri2004-AI <hs.shri2004@gmail.com>
2025-12-31 15:17:44 +0530

    in this lecture ,i learn formEvents

---
+++ Day9/formEvents/script.js
@@ -0,0 +1,16 @@
+let form=document.querySelector("form");
+
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    console.dir(form);
+
+    let user=this.elements[0];
+    let pass=this.elements[1];
+
+    console.log(user.value);
+    console.log(pass.value);
+
+    alert(`Hii ${user.value}, your password is set to ${pass.value}`);
+
+    
+});
