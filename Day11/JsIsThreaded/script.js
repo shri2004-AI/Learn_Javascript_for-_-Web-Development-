9dcf7151cde223a0556c56b4132c17e84ac91492
shri2004-AI <hs.shri2004@gmail.com>
2026-01-03 23:19:59 +0530

    in this lecture,i am learn js is single threaded

---
+++ Day11/JsIsThreaded/script.js
@@ -0,0 +1,22 @@
+// synchronus nature
+
+// let a=25;
+// console.log(a);
+// let b=10;
+// console.log(b);
+// console.log(a+b);
+
+
+
+
+
+setTimeout(()=>{
+    console.log("apna college");
+},2000);
+
+setTimeout(()=>{
+    console.log("apna ghar");
+},2000);
+
+
+console.log("hello harsh.....");
